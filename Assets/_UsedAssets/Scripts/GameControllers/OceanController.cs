b8c7823e58d49193bd533599d833b16addc79a60
Rock <yz915@nyu.edu>
2015-04-30 15:01:33 +0400

    before merge with Julia new UI

--- Assets/_UsedAssets/Scripts/GameControllers/OceanController.cs
+++ Assets/_UsedAssets/Scripts/GameControllers/OceanController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class OceanController : MonoBehaviour {
 	
@@ -7,17 +8,22 @@ public class OceanController : MonoBehaviour {
 	Vector3 targetSeaLevel;
 	
 	Vector3 highestSeaLevel;
-	
-	float risingSpeed=0.1f;
+	float risingSpeed=0.01f;
+
 	public float startingSeaLevel=150;
 
+	float sealevel_percentage;
+
+	public Image sealevel_Bar;
+
+	public Text SealevelPercentageText;
 
 	// Use this for initialization
 	void Start () {
 		targetSeaLevel = transform.position;
 		targetSeaLevel.y = startingSeaLevel;
-		
-		highestSeaLevel = targetSeaLevel;
+//		highestSeaLevel = targetSeaLevel;
+		highestSeaLevel = new Vector3 (0f,200f,0f);
 	}
 	
 	// Update is called once per frame
@@ -33,9 +39,21 @@ public class OceanController : MonoBehaviour {
 	{
 		targetSeaLevel.y = targetLevel;
 	}
-	
+
+	public void setRisingSpeed(float theSpeed){
+		Debug.Log ("the rising speeding is changed to " + theSpeed);
+		risingSpeed = theSpeed;
+	}
+
 	public void seaLevelUpdate()
 	{
 		transform.position = Vector3.Lerp (transform.position, targetSeaLevel, Time.deltaTime);
+
+		sealevel_percentage = targetSeaLevel.y / highestSeaLevel.y;
+		int percentage_number = (int) (sealevel_percentage * 100);
+
+		SealevelPercentageText.text = percentage_number.ToString ()+ "%";
+
+		sealevel_Bar.rectTransform.sizeDelta = new Vector2 (100, percentage_number);
 	}
 }
---
+++ Assets/_UsedAssets/Scripts/GameControllers/SnowBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startSnowing(int SnowSpeed){
+		
+		GetComponent<EllipsoidParticleEmitter> ().maxEmission = SnowSpeed;
+		
+		GetComponent<EllipsoidParticleEmitter> ().minEmission = SnowSpeed;
+
+	}
+}
